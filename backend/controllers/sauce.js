4fb1656cf4634f4a5dc4141876f6e8f412e66dcb
Jeremy Stievet <stievet.jeremy@gmail.com>
2022-10-13 13:15:02 +0200

    essaie de supprimer et modifier

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,14 +2,14 @@
 import Sauce from '../models/Sauce.js';
 export const createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject.id;
+    delete sauceObject._id;
     delete sauceObject._userId
     const sauce = new Sauce({
         ...sauceObject,
         userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    console.log('sauce', sauce);
+    
 
     sauce.save()
         .then(() => { res.status(201).json({ message: 'sauce enregistré !' }) })
@@ -23,12 +23,11 @@ export const modifySauce = (req, res, next) => {
 }
 
 export const deleteSauce = (req, res, next) => {
-    console.log('deletesauce');
-    Sauce.deleteOne({ _id: req.params.id })
+        Sauce.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'sauce supprimé !' }))
         .catch(error => res.status(400).json({ error }));
-}
-
+    }
+    
 export const getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
