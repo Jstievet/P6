8b3bd80112778f1c7ae49a9975f42b8139564ae7
Jeremy Stievet <stievet.jeremy@gmail.com>
2022-10-31 16:43:18 +0100

    ajout du readme

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -20,16 +20,18 @@ export const logIn = (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                console.log('test user');
-                res.status(401).json({ message: "Paire identifiant / Mot de passe incorrecte" });
+
+                return res.status(401).json({ message: "Paire identifiant / Mot de passe incorrecte" });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
+                        console.log('user', user);
                         console.log('test user mdp');
-                        res.status(401).json({ message: "Paire identifiant / Mot de passe incorrecte" })
+
+                        return res.status(401).json({ message: "Paire identifiant / Mot de passe incorrecte" })
+
                     }
-                    console.log('req', req.body);
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
@@ -39,7 +41,15 @@ export const logIn = (req, res) => {
                         )
                     })
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => {
+                    console.log('error', error);
+                    res.status(500).json({ error });
+                })
+        }
+            // .catch(error => res.status(500).json({ error }));
+        )
+        .catch(error => {
+            console.log('error', error);
+            res.status(500).json({ error });
         })
-        .catch(error => res.status(500).json({ error }));
 }
